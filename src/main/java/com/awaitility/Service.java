2b59c128d506f42e23b75098444d3d54cae0dca1
awaitility <mhudyma@wayfair.com>
2025-02-21 13:23:24 +0100

    init

---
+++ src/main/java/com/awaitility/Service.java
@@ -0,0 +1,12 @@
+package com.awaitility;
+
+public class Service {
+
+  public static String execute() {
+    return "execution";
+  }
+
+  public static String execute2() {
+    return "execution2";
+  }
+}
